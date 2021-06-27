2886c65c8ca7155a46c1a75cd39271a7d5edb59c
mahmood114 <mahmoodalmohri@gmail.com>
2021-06-27 17:43:41 +0300

    First commit

---
+++ src/components/WatchItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WatchItem = () => {
+    return (
+        <div>
+            <ul className="list-group-item">hello</ul>
+        </div>
+    );
+};
+
+export default WatchItem;
