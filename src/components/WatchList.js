37ed9c37facbf2d4af7546685b8f15b104b7cb28
mahmood114 <mahmoodalmohri@gmail.com>
2021-06-28 17:58:49 +0300

    Finished all features

--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -2,17 +2,23 @@ import React from "react";
 import WatchItem from "./WatchItem";
 import movieStore from "../MovieStore";
 import { observer } from "mobx-react";
+import {useState} from "react";
 
 const WatchList = (props) => {
+
+  const [query, setQuery] = useState("");
+
   const movs = movieStore.movies
     .filter(movie => movie.watched === props.watched)
+    .filter(movie => movie.title.toLowerCase().includes(query.toLowerCase()))
     .map((movie) => <WatchItem movie={movie} />);
 
   return (
     <div className="watch-list">
         <h3>{props.watched? "Watched" : "Watchlist"}</h3>
         <div className="search-bar">
-          <input placeholder="Search"/>
+          <input onChange={(event) => setQuery(event.target.value)} placeholder="Search"/>
+          <p>{movs.length} movie{movs.length>1? "s" : ""}</p>
         </div>
         <ul className="list-group list-group-flush">{movs}</ul>
     </div>
