9928cfcd6f9fab812484fcb10f8d98c2277e491f
mahmood114 <mahmoodalmohri@gmail.com>
2021-06-28 17:48:56 +0300

    5th Feature Done

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,13 +1,25 @@
 import movieStore from "../MovieStore";
+import {useState} from "react";
 
 const AddMovie = () => {
+  
+  const [movie, setMovie] = useState({
+    id: 0,
+    title: "",
+    watched: false,
+  });
+
   const handleAddition = () => {
-    movieStore.createMovie();
+    movieStore.createMovie(movie);
   };
+
+  const handleChange = (event) => {
+    setMovie({...movie, ["title"]: event.target.value})
+  }
 
   return (
     <div>
-      <input className="Add" placeholder="Add new Movie" />
+      <input onChange={handleChange}className="Add" placeholder="Add new Movie" />
       <button className="btn btn-primary" onClick={handleAddition}>
         Add
       </button>
